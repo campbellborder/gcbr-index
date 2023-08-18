d2816bdf63a5016a6fb2a91f3056ade90fd87573
Campbell Border <campbellborder64@gmail.com>
2023-08-18 19:18:09 +1000

    Added data api route not functional

--- src/app/api/data/[type]/route.ts
+++ src/app/api/data/[type]/route.ts
@@ -1,8 +1,14 @@
+import { NextResponse } from 'next/server';
+import path from 'path';
+let csvToJson = require('convert-csv-to-json')
+
 export async function GET(
     request: Request,
     { params }: {params: {type : string} }
 ) {
     const type = params.type
-
-    
+    const jsonDirectory = path.join(process.cwd(), 'data');
+    const json = csvToJson.getJsonFromCsv(jsonDirectory + "/dummy_data.csv")
+    const keys = Object.keys(json[0])
+    return NextResponse.json(type);
 }
--- src/app/api/geo/route.js
+++ src/app/api/geo/route.js
@@ -3,9 +3,8 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export async function GET() {
-  //Find the absolute path of the json directory
+  //Find the absolute path of the file and read
   const jsonDirectory = path.join(process.cwd(), 'data');
-  //Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/countries.geojson', 'utf8');
   //Return the content of the data file in json format
   return NextResponse.json(fileContents);
